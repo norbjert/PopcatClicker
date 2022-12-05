0d7536285778294bbe5247e3082682d24b0cc4f5
norbjert <norbert88@protonmail.com>
2022-12-05 14:27:34 +0100

    autoaccepts cookies, can work fully autonomous now

--- README.md
+++
@@ -1,8 +0,0 @@
-# PopcatClicker
-
-
-Up to 
-72 clicks per second
-4320 clicks per minute
-259,200 clicks per hour
-6,220,800 clicks per day
--- popcat.sh
+++ popcat.sh
@@ -2,4 +2,4 @@
 git pull
 #sudo apt install gradle -y
 gradle jar
-nohup java -jar "$(pwd)"/build/libs/discordSlave1-1.0.jar &
+nohup java -jar "$(pwd)"/build/libs/popcatClicker-0.0.1-SNAPSHOT-plain.jar &
